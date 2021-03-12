cd76ecb2be932ee920eac6df31c4adbe725c1680
Anastasia Gartseva <amgartseva@gmail.com>
2021-03-12 18:58:48 +0300

    first commit

--- script/index.js
+++ script/index.js
@@ -1,135 +1,137 @@
 document.addEventListener('DOMContentLoaded', () => {
     'use strict';
 
-    const crossSellList = document.querySelector('.cross-sell__list'),
-    modal = document.querySelector('.modal'),
-    cardDetailsTitle = document.querySelector('.card-details__title'),
-    modalSubtitle = document.querySelector('.modal__subtitle');
+    const crossSellList = document.querySelector('.swiper-wrapper'),
+        modal = document.querySelector('.modal'),
+        cardDetailsTitle = document.querySelector('.card-details__title'),
+        modalSubtitle = document.querySelector('.modal__subtitle');
 
     crossSellList.textContent = '';
 
     const getData = async () => await fetch('./cross-sell-dbase/dbase.json');
 
     const tabs = () => {
-        const cardDetailChange = document.querySelectorAll('.card-detail__change'),
-            cardImage = document.querySelector('.card__image_item'),
-            descriptionMemory = document.querySelector('.description__memory'),
-            cardDetailsPrice = document.querySelector('.card-details__price');
+            const cardDetailChange = document.querySelectorAll('.card-detail__change'),
+                cardImage = document.querySelector('.card__image_item'),
+                descriptionMemory = document.querySelector('.description__memory'),
+                cardDetailsPrice = document.querySelector('.card-details__price');
 
-        const data = [{
-                name: 'Смартфон Apple iPhone 12 Pro 128GB Graphite',
-                img: 'img/iPhone-graphite.png',
-                price: 95990,
-                memoryROM: 128
-            },
-            {
-                name: 'Смартфон Apple iPhone 12 Pro 128GB Silver',
-                img: 'img/iPhone-silver.png',
-                price: 120990,
-                memoryROM: 256
-            },
-            {
-                name: 'Смартфон Apple iPhone 12 Pro 128GB Pacific Blue',
-                img: 'img/iPhone-blue.png',
-                price: 99990,
-                memoryROM: 128
-            }
-        ];
-
-        const hideAll = () => {
-            cardDetailChange.forEach(item => item.classList.remove('active'));
-        };
-
-        cardDetailChange.forEach((tab, i) => {
-            tab.addEventListener('click', () => {
-                if (!tab.classList.contains('active')) {
-                    hideAll();
-
-                    cardDetailChange[i].classList.add('active');
-                    cardDetailsTitle.textContent = data[i].name;
-                    cardImage.setAttribute('src', data[i].img);
-                    cardDetailsPrice.textContent = `${data[i].price}₽`;
-                    descriptionMemory.textContent = `Встроенная память (ROM) ${data[i].memoryROM} ГБ`;
+            const data = [{
+                    name: 'Смартфон Apple iPhone 12 Pro 128GB Graphite',
+                    img: 'img/iPhone-graphite.png',
+                    price: 95990,
+                    memoryROM: 128
+                },
+                {
+                    name: 'Смартфон Apple iPhone 12 Pro 128GB Silver',
+                    img: 'img/iPhone-silver.png',
+                    price: 120990,
+                    memoryROM: 256
+                },
+                {
+                    name: 'Смартфон Apple iPhone 12 Pro 128GB Pacific Blue',
+                    img: 'img/iPhone-blue.png',
+                    price: 99990,
+                    memoryROM: 128
                 }
+            ];
+
+            const hideAll = () => {
+                cardDetailChange.forEach(item => item.classList.remove('active'));
+            };
+
+            cardDetailChange.forEach((tab, i) => {
+                tab.addEventListener('click', () => {
+                    if (!tab.classList.contains('active')) {
+                        hideAll();
+
+                        cardDetailChange[i].classList.add('active');
+                        cardDetailsTitle.textContent = data[i].name;
+                        cardImage.setAttribute('src', data[i].img);
+                        cardDetailsPrice.textContent = `${data[i].price}₽`;
+                        descriptionMemory.textContent = `Встроенная память (ROM) ${data[i].memoryROM} ГБ`;
+                    }
+                });
             });
-        });
-    }, 
-    accordion = () => {
-        const characteristics = document.querySelector('.characteristics'),
-            characteristicsItem = document.querySelectorAll('.characteristics__item');
+        },
+        accordion = () => {
+            const characteristics = document.querySelector('.characteristics'),
+                characteristicsItem = document.querySelectorAll('.characteristics__item');
 
-        const open = (button, dropDown) => {
-                closeAllDrops();
-                dropDown.style.height = `${dropDown.scrollHeight}px`;
-                button.classList.add('active');
-                dropDown.classList.add('active');
-            },
-            close = (button, dropDown) => {
-                button.classList.remove('active');
-                dropDown.classList.remove('active');
-                dropDown.style.height = ``;
-            }, closeAllDrops = () => {
-                if (Array.from(characteristicsItem).some(item => item.children[1].classList.contains('active'))) {
-                    characteristicsItem.forEach(item => {
-                        if (item.children[0].classList.contains('active')) {
-                            close(item.children[0], item.children[1]);
-                        }
-                    })
-                    
+            const open = (button, dropDown) => {
+                    closeAllDrops();
+                    dropDown.style.height = `${dropDown.scrollHeight}px`;
+                    button.classList.add('active');
+                    dropDown.classList.add('active');
+                },
+                close = (button, dropDown) => {
+                    button.classList.remove('active');
+                    dropDown.classList.remove('active');
+                    dropDown.style.height = ``;
+                },
+                closeAllDrops = () => {
+                    if (Array.from(characteristicsItem).some(item => item.children[1].classList.contains('active'))) {
+                        characteristicsItem.forEach(item => {
+                            if (item.children[0].classList.contains('active')) {
+                                close(item.children[0], item.children[1]);
+                            }
+                        })
+
+                    }
+                }
+
+            characteristics.addEventListener('click', e => {
+                const target = e.target;
+
+                if (target.classList.contains('characteristics__title')) {
+                    const parent = target.closest('.characteristics__item'),
+                        description = parent.querySelector('.characteristics__description');
+
+                    description.classList.contains('active') ? close(target, description) : open(target, description);
+                };
+
+                if (!target.classList.contains('characteristics__title') && !target.closest('.characteristics__description')) {
+                    closeAllDrops();
+                };
+            });
+
+            document.body.addEventListener('click', e => {
+                const target = e.target;
+
+                if (!target.classList.contains('characteristics__title') && !target.closest('.characteristics__description')) {
+                    closeAllDrops();
+                };
+            });
+        },
+        modalFunc = () => {
+            const cardDetailsButtonBuy = document.querySelector('.card-details__button_buy'),
+                cardDetailsButtonDelivery = document.querySelector('.card-details__button_delivery');
+
+            const closeModal = e => {
+                const target = e.target;
+
+                if (target === modal || target.classList.contains('modal__close') || e.code === 'Escape') {
+                    modal.classList.remove('open');
                 }
             }
 
-        characteristics.addEventListener('click', e => {
-            const target = e.target;
+            cardDetailsButtonBuy.addEventListener('click', () => {
+                openModal('Оплата', cardDetailsTitle.textContent);
+            });
 
-            if (target.classList.contains('characteristics__title')) {
-                const parent = target.closest('.characteristics__item'),
-                    description = parent.querySelector('.characteristics__description');
+            cardDetailsButtonDelivery.addEventListener('click', () => {
+                openModal('Доставка и оплата')
+            });
 
-                description.classList.contains('active') ? close(target, description) : open(target, description);
-            };
+            modal.addEventListener('click', closeModal);
 
-            if (!target.classList.contains('characteristics__title') && !target.closest('.characteristics__description')) {
-                closeAllDrops();
-            };
-        });
-
-        document.body.addEventListener('click', e => {
-            const target = e.target;
-
-            if (!target.classList.contains('characteristics__title') && !target.closest('.characteristics__description')) {
-                closeAllDrops();
-            };
-        });
-    },
-    modalFunc = () => {
-        const cardDetailsButtonBuy = document.querySelector('.card-details__button_buy'),
-        cardDetailsButtonDelivery = document.querySelector('.card-details__button_delivery');
-
-        const closeModal = e => {
-            const target = e.target;
-
-            if (target === modal || target.classList.contains('modal__close') || e.code === 'Escape') {
-                modal.classList.remove('open');
-            }
-        }
-
-        cardDetailsButtonBuy.addEventListener('click', () => {
-            openModal('Оплата', cardDetailsTitle.textContent);
-        });
-
-        cardDetailsButtonDelivery.addEventListener('click', () => {
-            openModal('Доставка и оплата')
-        });
-
-        modal.addEventListener('click', closeModal);
-
-        document.body.addEventListener('keydown', closeModal);
-    }, openModal = (text, title) => {
-        modalSubtitle.textContent = text;
-        document.querySelector('.modal__title').textContent = title;
-        modal.classList.add('open');
-    };
+            document.body.addEventListener('keydown', closeModal);
+        },
+        openModal = (text, title) => {
+            modalSubtitle.textContent = text;
+            document.querySelector('.modal__title').textContent = title;
+            modal.classList.add('open');
+        };
 
     tabs();
     accordion();
@@ -146,7 +148,7 @@ document.addEventListener('DOMContentLoaded', () => {
         .then(data => {
             data.forEach(item => {
                 crossSellList.insertAdjacentHTML('beforeend', `
-                <li id="${item.id}">
+                <li id="${item.id}" class="swiper-slide">
                     <article class="cross-sell__item">
                         <img class="cross-sell__image" src="${item.photo}" alt="${item.name}">
                         <h3 class="cross-sell__title">${item.name}</h3>
@@ -162,10 +164,18 @@ document.addEventListener('DOMContentLoaded', () => {
             return crossSellItem;
         })
         .then(sellItems => {
-            sellItems.forEach(item => 
+            const swiper = new Swiper('.cross-sell__list', {
+                slidesPerView: 4,
+                spaceBetween: 10,
+                loop: true,
+                autoplay: {
+                    delay: 2000,
+                },
+            });
+            sellItems.forEach(item =>
                 item.addEventListener('click', e => {
                     const target = e.target;
-    
+
                     if (target.classList.contains('cross-sell__button')) {
                         openModal('Оплатить', item.querySelector('.cross-sell__title').textContent);
                     }
